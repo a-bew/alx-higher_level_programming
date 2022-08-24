ee49cd7fccd00aa784921914708855fe570bebe8
Adetola Bewaji <adetola013@gmail.com>
2022-08-24 01:46:42 -0700

    updated

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for char in range(26):
-    print(
-            "{}".format(chr(ord("z") - char) if char % 2 == 0
-    else chr(ord("z") - 32 - char)), end=""i)
+    print("{}".format(chr(ord("z") - char) if char % 2 == 0
+        else chr(ord("z") - 32 - char)), end=""i)
