cc5fa7b263318792426e30b1801af4331f8edbd3
Adetola Bewaji <adetola013@gmail.com>
2022-08-24 01:12:19 -0700

    pycode style

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for char in range(26):
     print(
-            "{}".format(chr(ord("z") - char) 
-                if char % 2 == 0 
-                else chr(ord("z") -32 - char)) , end=""
+            "{}".format(chr(ord("z") - char)
+                if char % 2 == 0
+                else chr(ord("z") - 32 - char)), end=""
             )
