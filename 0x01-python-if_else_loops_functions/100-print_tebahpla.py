5c207951e57de479b46f93e764d61e0b688f44a7
Adetola Bewaji <adetola013@gmail.com>
2022-08-24 01:36:12 -0700

    indentation corrected

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,4 +2,4 @@
 for char in range(26):
     print(
             "{}".format(chr(ord("z") - char) if char % 2 == 0
-        else chr(ord("z") - 32 - char)), end="")
+    else chr(ord("z") - 32 - char)), end=""i)
