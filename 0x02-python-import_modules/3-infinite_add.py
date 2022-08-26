85d42fbe558c5d39474e5ffb82211c4a7b9366c4
Adetola Bewaji <adetola013@gmail.com>
2022-08-25 21:20:30 -0700

    infinite_add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,7 +5,6 @@ if (__name__ == "__main__"):
     i = 1
     result = 0
     arguments = len(argv) - 1
-
     if (arguments != 0):
         while (i <= arguments):
             result += int(argv[i])
