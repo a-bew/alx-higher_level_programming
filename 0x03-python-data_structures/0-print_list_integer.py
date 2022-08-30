e3432f50688be8fab3c0a007a12dee7220900d9b
Adetola Bewaji <adetola013@gmail.com>
2022-08-29 20:43:56 -0700

    another updates

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for integer in my_list:
-        print("{}\n".format(integer))
+    for i in range(len(my_list)):
+        print("{:d}\n".format(my_list[i]))
