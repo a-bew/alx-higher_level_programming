29b2e545c2cfa05d33b1862d640e91c5f0f0878f
Adetola Bewaji <adetola013@gmail.com>
2022-08-29 21:01:03 -0700

    print_reversed_list_integer

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
 
-def element_at(my_list, idx):
-    if (idx < 0):
-        return None
-    elif (idx+1 > len(my_list)):
-        return None
-    return(my_list[idx])
+    if not my_list:
+        pass
+    else:
+        my_list.reverse()
+        for i in range(len(my_list)):
+            print("{:d}".format(my_list[i]))
