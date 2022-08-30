42dd1c5bb16867ae1c4aad7c7b351c35b164ba4e
Adetola Bewaji <adetola013@gmail.com>
2022-08-29 21:03:04 -0700

    if statement added

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for n in my_list):
-        print("{:d}\n".format(n))
+def print_reversed_list_integer(my_list=[]):
+
+    if not my_list:
+        pass
+    else:
+        for i in range(len(my_list)):
+            print("{:d}".format(my_list[i]))
