30e56b841598aadb3bcd2d02c547fc10fd3c1006
Adetola Bewaji <adetola013@gmail.com>
2022-08-29 21:06:17 -0700

    new in list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    if (idx < 0):
+        return (my_list)
+    elif (idx+1 > len(my_list)):
+        return (my_list)
+    my_list1 = my_list.copy()
+    my_list1[idx] = element
+    return (my_list1)
