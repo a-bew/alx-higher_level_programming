3f5434e071233082e096a634ad4d6cd7309d0180
Adetola Bewaji <adetola013@gmail.com>
2022-08-31 21:43:53 -0700

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    return max(a_dictionary, key=a_dictionary.get) if my_dict else None
+    return max(a_dictionary, key=a_dictionary.get) if a_dictionry else None
 
