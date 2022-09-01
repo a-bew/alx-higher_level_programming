83a72f593be90dd1b0acfd6da5fe9be94a9045ef
Adetola Bewaji <adetola013@gmail.com>
2022-08-31 20:47:20 -0700

    print sorted dictionary updates

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    return {key:value for key, value in sorted(a_dictionary.items())}
+    for k in sorted(a_dictionary.keys()):
+        print("{}: {}".format(k, a_dictionary[k]))
