011338381532dd2064147de3d20a4123e36dbdba
Adetola Bewaji <adetola013@gmail.com>
2022-08-31 20:11:49 -0700

    doesn't copy

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    my_list_copy = my_list
     for  i in range(len(my_list)):
         if search == my_list[i]:
-            my_list_copy[i] = replace
-    return my_list_copy
+            my_list[i] = replace
+    return my_list
