2a8c1f753b082209b4ce31620117eddb7b1a5c1c
Adetola Bewaji <adetola013@gmail.com>
2022-09-13 11:41:46 -0700

    newly updated

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,8 +10,8 @@ class Square():
     """Square implementation
     """
     def __init__(self, size=0):
-        if type(value) != int:
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
         self.__size = size
