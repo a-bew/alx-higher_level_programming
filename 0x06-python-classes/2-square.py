833cb789609ae00206b3fbb57a154a18f95d917c
Adetola Bewaji <adetola013@gmail.com>
2022-09-13 11:35:02 -0700

    size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,8 +8,8 @@ class Square():
     """This class is a Size validation for Square
     """
     def __init__(self, size=0):
-        self.__size = size
         if type(value) is not int:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
