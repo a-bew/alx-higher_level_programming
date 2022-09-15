b41624a2746d914b74b5f4059accd1a329cc6e78
Adetola Bewaji <adetola013@gmail.com>
2022-09-14 20:56:35 -0700

    updated

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,11 +3,11 @@ import sys
 
 
 def safe_print_integer_err(value):
-    check = True
+    
     try:
         print("{:d}".format(value))
     except (ValueError,TypeError) as error:
         print('Exception: {}'.format(error), file=sys.stderr)
-        check = False
-    finally:
-        return (check)
+        return False
+
+    return True
