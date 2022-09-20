b72c0815721159a0df39bab2d1539708752def3a
Adetola Bewaji <adetola013@gmail.com>
2022-09-19 22:15:21 -0700

    updated

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -40,7 +40,7 @@ class Rectangle:
         if self.__height == 0 or self.width == 0:
             return total
         for i in range(self.__height):
-            total += (str(print_symbol) * self.__width)
+            total += (str(self.print_symbol) * self.__width)
             if i is not self.__height - 1:
                 total += "\n"
         return total
