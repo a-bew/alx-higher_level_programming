4baa3a1a118b7c5b9274f742174750aa7166308b
Adetola Bewaji <adetola013@gmail.com>
2022-09-26 14:25:20 -0700

    is_same_class method

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This program return Boolean isinstance of same class"""
+
+
+def is_same_class(obj, a_class):
+    """get the method of is_same_class"""
+    return isinstance(obj, a_class)
