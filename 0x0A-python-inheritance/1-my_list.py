6566de8d6a59c0c66435bbdaa5a80517c947c2b4
Adetola Bewaji <adetola013@gmail.com>
2022-09-26 14:14:40 -0700

    sorted list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module defines a MyList Object.
+"""
+
+class MyList(list):
+    """
+    MyList class skeleton
+    """
+    def print_sorted(self):
+        """"
+        Sort
+        Return: self
+        """
+        super().sort()
+        print(self)
