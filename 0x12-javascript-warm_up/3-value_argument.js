132fe719e115f527fa058eda761e159a8eca321e
Adetola Bewaji <adetola013@gmail.com>
2022-12-11 14:05:42 -0800

    Add script to print first argument

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,11 @@
+#!/user/bin/node
+
+// Get the first argument passed to the script
+const firstArg = process.argv[2];
+
+// Check if an argument was provided
+if (firstArg === undefined) {
+  console.log('No argument');
+} else {
+  console.log(firstArg);
+}
